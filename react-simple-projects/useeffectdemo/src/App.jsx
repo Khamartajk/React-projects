ed36fe31da4cd577e685fe724110a8b4f85f0c3e
Khamartaj <kkalegar25@gmail.com>
2025-02-02 01:11:41 +0530

    API Calling done

--- react-simple-projects/useeffectdemo/src/App.jsx
+++ react-simple-projects/useeffectdemo/src/App.jsx
@@ -1,16 +1,34 @@
-import React, {useState} from 'react'
-import './App.css'
-import CounterApp from './components/CounterApp';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import CounterApp from "./components/CounterApp";
+import axios from "axios";
 
 function App() {
-  const [state, setState] = useState(true);
+  const [todos, setTodos] = useState([]);
 
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => setTodos(response.data));
+  }, []);
   return (
     <>
-     <button onClick={(e) => setState(!state)}>Toggle Counter</button><hr />
-     {state ? <CounterApp /> : null}  
+      <div className="container">
+        <h1>Todo List</h1>
+        <hr />
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              <p>{todo.id}</p>,{todo.title},{todo.completed}
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
-  )
+
+    /* <button onClick={(e) => setState(!state)}>Toggle Counter</button><hr />
+     {state ? <CounterApp /> : null}   */
+  );
 }
 
 export default App;
--- react-simple-projects/useeffectdemo/src/components/CounterApp.jsx
+++ react-simple-projects/useeffectdemo/src/components/CounterApp.jsx
@@ -1,45 +1,57 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react' 
 import '../App.css'
+import { use } from 'react'
 
 
 function CounterApp() {
-   const [count, setCount] = useState(0)  
+  //  const [count, setCount] = useState(0)  
 
-   useEffect(() => {
-      console.log ("counter mounted")
-      return () => {
-         console.log ("counter unmounted")
-      } 
-   },[])
+  
+  //  useEffect(() => {
+  //     console.log ("counter mounted")
+  //     return () => {
+  //        console.log ("counter unmounted")
+  //     } 
+  //  },[])
 
-   useEffect(() => {
-      console.log ("counter updated")
-   },[count]) 
+  //  useEffect(() => {
+  //     console.log ("counter updated")
+  //  },[count]) 
 
-   const handleIncrement = () => {
-      setCount(count + 1)
-   }
+  //  const handleIncrement = () => {
+  //     setCount(count + 1)
+  //  }
 
-   const handleDecrement = () => {
-      if (count === 0) {
-         return
-      }
-      setCount(count - 1)
-   }
+  //  const handleDecrement = () => {
+  //     if (count === 0) {
+  //        return
+  //     }
+  //     setCount(count - 1)
+  //  }
 
 
   return (
-    <div className="counter">
-      <h2>{count}</h2>
-      <hr />  
-      
-      <button style={{backgroundColor: 'seagreen', color: 'white'}}
-      onClick={handleIncrement}>Increment</button>
-      <button style={{backgroundColor: 'tomato', color: 'white'}}
-       onClick={handleDecrement}>Decrement</button>  
+    
+    <>
+       
+  
+  
+  
+  
+    </>
 
-    </div>
+    // <div className="counter">
+    //   <h2>{count}</h2>
+    //   <hr />  
+      
+    //   <button style={{backgroundColor: 'seagreen', color: 'white'}}
+    //   onClick={handleIncrement}>Increment</button>
+    //   <button style={{backgroundColor: 'tomato', color: 'white'}}
+    //    onClick={handleDecrement}>Decrement</button>  
+
+    // </div>
+    
   )
 }
 
